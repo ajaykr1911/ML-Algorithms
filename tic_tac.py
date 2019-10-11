6b43c96210ff2a84e20375870aa63967e3ae76fc
Ajay Kumar <54458384+ajaykr1911@users.noreply.github.com>
2019-10-11 21:12:33 +0530

    Update tic_tac.py

--- tic_tac.py
+++ tic_tac.py
@@ -1,11 +1,15 @@
 import numpy as np
 from random import randint
-zero_mat = np.zeros([3,3], dtype=int)
-while True:
+zero_mat = np.zeros([3,3], dtype=int)     # creating a zero matrix
+
+while True:     # initialize a loop until any player wins
+
+    # taking matrix index i and j for user move 
     i,j = int(input()), int(input())
     zero_mat[i][j] = 1
     print('matrix after user move: \n',zero_mat)
 
+    # computer move
     while True:
         k,l = randint(0,2), randint(0,2)
         if zero_mat[k][l] == 0 :
@@ -14,8 +18,9 @@ while True:
 
     print('matrix after computer move: \n',zero_mat)
 
-    count = 0
+    count = 0  # if any player wins count = 1 and the outer while loop will break
 
+    # checking if any player is winning by completing cross direction 
     sum = zero_mat[0][0]+zero_mat[1][1]+zero_mat[2][2]
     if sum == 3:
         print("user is winner")
@@ -35,7 +40,8 @@ while True:
         print('you lose')
         count = 1
         break
-
+        
+    # checking if any player is winning by completing a row
     for i in range(3):
         sum = 0
         for j in range(3):
@@ -48,7 +54,8 @@ while True:
                 print('you lose')
                 count = 1
                 break
-
+                
+    # checking if any player is winning by completing a column
     for i in range(3):
         sum = 0
         for j in range(3):
@@ -61,5 +68,6 @@ while True:
                 print('you lose')
                 count = 1
                 break
+      # condition for exit the outer loop          
     if count == 1:
         break       
